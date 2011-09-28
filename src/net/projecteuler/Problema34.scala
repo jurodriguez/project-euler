0ff53a60e8d70ce4d0517c9bd4ed7fd2e83e21d4
Juan Rodríguez <juan.pj@gmail.com>
2011-09-27 23:42:01 -0500

    Solución al problema 34.

---
+++ src/net/projecteuler/Problema34.scala
@@ -0,0 +1,18 @@
+def fact(n:Int):Int = {
+	if (n==0) 
+		return 1 
+	else 
+		return n * fact(n-1)
+}
+
+var sum = 0;
+for(i <- 10 until 100000){
+	var sumOfDigits = 0;
+	for(c <- i.toString){
+		sumOfDigits += fact(c.toString.toInt)
+	}
+	if(i == sumOfDigits){
+		sum += i
+	}
+}
+println("sum="+sum)
